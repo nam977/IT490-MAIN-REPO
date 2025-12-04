b5ea013adbafb54f8a8e6c2c0502255087b99efc
aag76 <aag76@njit.edu>
2025-12-04 17:24:27 -0500

    Initialize database connection and RabbitMQ server
    Added database connection and RabbitMQ server initialization.
    Copied over relevant code from auth_worker.php for easy access

--- DATABASE_MQ_AND_BROKER/DATABASE_MQ/sample/stock_worker.php
+++ DATABASE_MQ_AND_BROKER/DATABASE_MQ/sample/stock_worker.php
@@ -1,1 +1,30 @@
+#!/user/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
+require_once __DIR__ . '/vendor/autoload.php';
+$client_ip = $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1';
+
+// Database credentials
+$db_host = '127.0.0.1';
+$db_user = 'testuser';
+$db_pass = 'rv9991$#';
+$db_name = 'testdb';
+
+// Connect to MySQL
+$mydb = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($mydb->connect_errno != 0) {
+    echo "Failed to connect to database: " . $mydb->connect_error . PHP_EOL;
+    exit(0);
+}
+
+
+$stockServer = new rabbitMQServer("testRabbitMQ.ini","sharedServer2");
+echo "Authentictation Server ready and on standby..." . PHP_EOL;
+$stockServerPid = pcntl_fork();
+if ($stockServerPid == 0){
+    $stockServer->process_requests('requestProcessor');
+    exit();
+}
