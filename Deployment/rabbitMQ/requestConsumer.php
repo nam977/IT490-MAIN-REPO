7e728e7b9fd4ff46dd2c7f5140d5b0bea9225848
it490admin <it490admin@IT490.tail7715b.ts.net>
2025-12-10 23:18:54 +0000

    fixed up consumer, combined the proposed two consumers into one file that checks which type of request it is through the msg

---
+++ Deployment/rabbitMQ/requestConsumer.php
@@ -0,0 +1,65 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'deployment', 'deployment');
+$channel = $connection->channel();
+
+$channel->queue_declare('deployment', false, true, false, false);
+
+echo " [*] Waiting for download/upload requests...\n";
+
+$callback = function (AMQPMessage $msg) {
+    $data = json_decode($msg -> getBody());
+    echo ' [x] Received ', $msg->getBody(), "\n";
+    $msg -> ack();
+
+
+  //first checking the message if it is valid
+
+    // "asker" => $user,
+    // "file" => "",
+    // "version" => "", (only in download)
+    // "action" => "download"
+    //
+    
+    $asker = $data->asker;
+    $file = $data->file;
+    $action = $data->action;
+
+    if ($action == "upload"){
+        //UPLOAD
+        //if valid request, sftp, and update database
+        
+
+
+    } else if ($action == "download") {
+        //DOWNLOAD
+        //if valid request then check database
+        $version = $data->version;
+        
+
+        //if database does not have then return error message
+    } else {
+        echo "Invalid request.";
+    }
+
+
+
+
+};
+
+$channel->basic_consume('deployment', '', false, false, false, false, $callback);
+
+
+
+
+while ($channel->is_consuming()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
+
+?>
